c36021c5aaadfa8a8d73022dfc458098f25d8b22
kits.av <amdkitsav@gmail.com>
2026-05-08 18:36:33 +0530

    Create 1034-SubarraysWithKDifferentIntegers

---
+++ 1034-SubarraysWithKDifferentIntegers/1034-SubarraysWithKDifferentIntegers.cpp
@@ -0,0 +1,25 @@
+// Last updated: 5/8/2026, 6:36:33 PM
+class Solution {
+public:
+    int slidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        int i = 0, j = 0, count = 0;
+        unordered_map<int, int> mp;
+
+        while (j < n) {
+            mp[nums[j]]++;
+            while (mp.size() > k) {
+                mp[nums[i]]--;
+                if (!mp[nums[i]])
+                    mp.erase(nums[i]);
+                i++;
+            }
+            count += j - i + 1;
+            j++;
+        }
+        return count;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return slidingWindow(nums, k) - slidingWindow(nums, k - 1);
+    }
+};
