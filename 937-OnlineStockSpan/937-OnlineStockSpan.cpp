0316b219063dc6d66c38f73119065b7394cb7043
kits.av <amdkitsav@gmail.com>
2026-05-08 18:36:38 +0530

    Create 937-OnlineStockSpan

---
+++ 937-OnlineStockSpan/937-OnlineStockSpan.cpp
@@ -0,0 +1,24 @@
+// Last updated: 5/8/2026, 6:36:38 PM
+class StockSpanner {
+public:
+    StockSpanner() {
+        
+    }
+
+    stack<pair<int,int>> s;
+    int next(int price) {
+        int res = 1;
+        while (!s.empty() && s.top().first <= price) {
+            res += s.top().second;
+            s.pop();
+        }
+        s.push({price, res});
+        return res;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
